3da96d9fb890eea45b824a869b6b7fbdc96d72eb
Jason Quach <jason@lungvang.com>
2018-07-08 13:18:43 +0700

    Reset repo to empty

--- _next/c04e7b28-b8ec-4955-afa2-0817b8d31947/page/about.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:webpackJsonp([4],{224:function(e,t,n){e.exports=n(225)},225:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var u=n(1),a=n.n(u),o=n(79);t.default=function(){return a.a.createElement(o.a,null,a.a.createElement("p",null,"This is the about page"))}}},[224]).default}});
