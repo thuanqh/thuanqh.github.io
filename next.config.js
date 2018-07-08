2938b40423b9eefb8337963d326b2817bbd6e401
Jason Quach <jason@lungvang.com>
2018-07-08 13:59:43 +0700

    Deploy personal blog to github pages

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const nexteinConfig = require('nextein/config').default
-
-module.exports = nexteinConfig({
-    // place our next config in here!
-})
--- posts/second-post.md
+++
@@ -1,5 +0,0 @@
----
-title: Second Post
----
-
-This is your second post. Now go and create an awesome blog!
