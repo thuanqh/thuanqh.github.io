1e9eff5a5db55338830e88b995fafef03181ef6b
Jason Quach <thuanqh@gmail.com>
2019-08-12 09:08:32 +0700

    Init all configuration to build porfolio with gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,37 @@
-plugins: [`gatsby-plugin-sass`];
+module.exports = {
+  siteMetadata: {
+    title: `Jason Portfolio`,
+    description: `Jason Portfolio`,
+    author: `jason@lungvang.com`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-styled-components",
+      option: {}
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/img`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jason Quach`,
+        short_name: `JQ`,
+        start_url: `/`,
+        background_color: `#333`,
+        theme_color: `#6b8e23`,
+        display: `standalone`,
+        icon: `src/assets/img/lungvang-icon.png`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import React from "react";
+import "../assets/scss/main.scss";
 
 export default () => <h1>Hello Jason Quach!</h1>;
