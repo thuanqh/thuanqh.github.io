806b77706739b1df0af8a9b7c199d5043e9968c6
Jason Quach <thuanqh@gmail.com>
2019-10-15 21:39:48 +0700

    Update layout and packages

---
+++ src/components/layout.js
@@ -0,0 +1,45 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+
+export default props => (
+  <div
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh"
+    }}
+  >
+    <header
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center"
+      }}
+    >
+      Header content
+    </header>
+    <main
+      sx={{
+        width: "100%",
+        flex: "1 1 auto"
+      }}
+    >
+      <div
+        sx={{
+          maxWidth: 768,
+          mx: "auto",
+          px: 3
+        }}
+      >
+        {props.children}
+      </div>
+    </main>
+    <footer
+      sx={{
+        width: "100%"
+      }}
+    >
+      Footer content
+    </footer>
+  </div>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <h1>Welcome to Jason's Tutorial</h1>
+  </Layout>
+);
