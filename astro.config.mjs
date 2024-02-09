e7a919a08939e06e19381efe2635abf74f738c78
Jason Quach <thuanqh@gmail.com>
2024-02-09 12:52:20 +0700

    Config tailwindcss with BaseStyles false

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thuanqh.github.io",
-  integrations: [tailwind(), vue()],
-});
+	site: 'https://thuanqh.github.io',
+	integrations: [
+		tailwind({
+			applyBaseStyles: false
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					includeAbsolute: false
+				}
+			}
+		})
+	]
+})
